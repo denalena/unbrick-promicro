d6fbc3f2603181beb0fc2c2d5e5007dd425f7971
deniz <github@ledeniz.de>
2023-04-25 18:17:44 +0200

    initial commit

---
+++ flash.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ef
+
+# Configuration ##############################
+FILE=./init.ino.hex # default compiled sketch; can be overwritten by calling the script with a filepath
+PORT=/dev/ttyACM0 # default port
+
+# Sparkfun ProMicro 5V 16MHz specific settings
+CONF=./conf/avrdude-sparkfun-promicro.conf
+DEVICE=atmega32u4
+PROGRAMMER=avr109
+BAUD=57600
+
+##############################################
+
+if [[ "$1" ]]; then
+	FILE="$1"
+fi
+
+if [[ ! -a "$PORT" ]]; then
+	echo "error: missing port: '${PORT}'"
+	exit 1
+fi
+
+if [[ ! -a "$FILE" ]]; then
+	echo "error: missing file: '${FILE}'"
+	exit 2
+fi
+
+##############################################
+# Flags below copied from Arduino IDE logs, April 2023
+# Run `avrdude -h` for descriptions
+
+./bin/avrdude \
+	-C${CONF} \
+	-v \
+	-V \
+	-p${DEVICE} \
+	-c${PROGRAMMER} \
+	-P${PORT} \
+	-b${BAUD} \
+	-D \
+	-Uflash:w:${FILE}:i
+
